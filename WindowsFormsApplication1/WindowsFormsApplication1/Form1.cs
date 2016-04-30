4c7297fcf3f6e6ad454ce6554bb5832fafdcac96
Ansh Sancheti <ansh.sancheti@gmail.com>
2016-04-29 23:02:17 -0700

    Added name to form

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class Schedlr : Form
     {
-        public Form1()
+        public Schedlr()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
