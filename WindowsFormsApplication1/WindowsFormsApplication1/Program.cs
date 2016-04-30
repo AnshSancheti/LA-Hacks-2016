6f7ea50e668bfcf291e40f78e62b5c1310d455c2
James Jiang <jmendre@berkeley.edu>
2016-04-30 02:07:31 -0700

    getevent

--- WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -40,12 +40,12 @@ namespace CalendarQuickstart
 {
     class Program
     {
-        
+
         // If modifying these scopes, delete your previously saved credentials
         // at ~/.credentials/calendar-dotnet-quickstart.json
         static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
         static string ApplicationName = "Google Calendar API .NET Quickstart";
-
+        
         static void Main(string[] args)
         {
             Application.EnableVisualStyles();
@@ -53,7 +53,7 @@ namespace CalendarQuickstart
             Application.Run(new Schedlr());
 
             UserCredential credential;
-
+            
             using (var stream =
                 new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
             {
@@ -93,11 +93,16 @@ namespace CalendarQuickstart
                 foreach (var eventItem in events.Items)
                 {
                     string when = eventItem.Start.DateTime.ToString();
+                    string end = eventItem.End.DateTime.ToString();
                     if (String.IsNullOrEmpty(when))
                     {
                         when = eventItem.Start.Date;
+                        end = eventItem.End.Date;
+                        
                     }
-                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                    
+                    Console.WriteLine("{0} ({1})", eventItem.Summary, when + " " + end);
+                    
                 }
             }
             else
@@ -105,33 +110,75 @@ namespace CalendarQuickstart
                 Console.WriteLine("No upcoming events found.");
             }
             Console.Read();
-
+            
         }
 
-        public void addEvent(String summary, String location, EventDateTime startDate, EventDateTime endDate)
+        public void getEvent(List<string> eventlist, List<string> location, List<string> starttime, List<string> endtime)
         {
-            Event myEvent = new Event
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
             {
-                Summary = summary,
-                Location = location,
-                Start = startDate, /* new EventDateTime()
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
                 {
-                    DateTime = new DateTime(2014, 6, 2, 10, 0, 0),
-                    TimeZone = "America/Los_Angeles"
-                }*/
-                End = endDate /* new EventDateTime()
-                {
-                    DateTime = new DateTime(2014, 6, 2, 10, 30, 0),
-                    TimeZone = "America/Los_Angeles"
-                },*/
-                /*Recurrence = recurrence, new String[] {
-                    "RRULE:FREQ=WEEKLY;BYDAY=MO"
-                },
-                Attendees = new List<EventAttendee>()
-                {
-                    new EventAttendee() { Email = "johndoe@gmail.com" }
-                }*/
-            };
+                    string when = eventItem.Start.DateTime.ToString();
+                    string end = eventItem.End.DateTime.ToString();
+                    string loc = " ";
+                    if (eventItem.Location != null)
+                    {
+                        loc = eventItem.Location.ToString();
+
+                    }
+                    
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                        end = eventItem.End.Date;
+                        
+                    }
+                    eventlist.Add(eventItem.Summary);
+                    starttime.Add(when);
+                    endtime.Add(end);
+                    location.Add(loc);
+
+                }
+            }
+            else
+            {
+            }
+            
 
             //myEvent.
             //Event recurringEvent = Events.Insert(myEvent, "primary").Execute();
